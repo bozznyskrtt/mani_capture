26148dd864938bc8fc6bbb46dddc68184c9c2f64
Haruki Isono <haruki.isono861@gmail.com>
2026-05-05 12:48:12 +0900

    fix: setup.py & setup.cfg

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+import os
+from glob import glob
+from pathlib import Path
 from setuptools import find_packages, setup
 
 package_name = 'mani_capture'
@@ -5,13 +8,12 @@ package_name = 'mani_capture'
 setup(
     name=package_name,
     version='1.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/mani_capture.launch.py',
-                                               'launch/mani_postprocess.launch.py']),
+        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -19,11 +21,7 @@ setup(
     maintainer_email='bozznyskrtt@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             # Main capture node used by mani_capture.launch.py
